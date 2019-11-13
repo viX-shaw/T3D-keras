7505629b0dd5ca765260fc2a66e7363bf852d63d
viX-shaw <songokurocks551@gmail.com>
2019-11-13 13:27:10 +0530

    wip

--- train_T3D_keras.py
+++ train_T3D_keras.py
@@ -107,18 +107,18 @@ def train():
     #TF.data with generator to work with TPU mirrored stratergy
     gn = tf.data.Dataset.from_generator(
         generator_fn_train,
-        ([tf.float32, tf.float32], tf.float32),
-        ([tf.TensorShape([BATCH_SIZE, FRAMES_PER_VIDEO, 224,224,3]), 
-        tf.TensorShape([BATCH_SIZE, FRAMES_PER_VIDEO, 256, 256, 3])],
-        tf.TensorShape([1])))
+        ([tf.float32, tf.float32], tf.float32))
+        # ([tf.TensorShape([BATCH_SIZE, FRAMES_PER_VIDEO, 224,224,3]), 
+        # tf.TensorShape([BATCH_SIZE, FRAMES_PER_VIDEO, 256, 256, 3])],
+        # tf.TensorShape([])))
         # (d_train, FRAMES_PER_VIDEO, FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNEL, nb_classes, BATCH_SIZE))
 
     gn_test = tf.data.Dataset.from_generator(
         generator_fn_test,
-        ([tf.float32, tf.float32], tf.float32),
-        ([tf.TensorShape([BATCH_SIZE, FRAMES_PER_VIDEO, 224,224,3]), 
-        tf.TensorShape([BATCH_SIZE, FRAMES_PER_VIDEO, 256, 256, 3])],
-        tf.TensorShape([1])))
+        ([tf.float32, tf.float32], tf.float32))
+        # ([tf.TensorShape([BATCH_SIZE, FRAMES_PER_VIDEO, 224,224,3]), 
+        # tf.TensorShape([BATCH_SIZE, FRAMES_PER_VIDEO, 256, 256, 3])],
+        # tf.TensorShape([])))
         # (d_train, FRAMES_PER_VIDEO, FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNEL, nb_classes, BATCH_SIZE))
         
     history = model.fit( #fit_generator does not work with distributed stratergy
