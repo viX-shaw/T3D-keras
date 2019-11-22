1c486ec39a81dd180b1911782e1d746a441faebf
viX-shaw <songokurocks551@gmail.com>
2019-11-22 14:13:47 +0530

    wip

--- train_T3D.py
+++ train_T3D.py
@@ -22,6 +22,7 @@ parser.add_argument("--epochs", type=int, default=200, help="epochs, times you w
 parser.add_argument("--use_multiprocessing", type=str, default="False", help="use mulitple processes")
 parser.add_argument("--pre-trained", type=str, default="", help="2d->3d transfer model")
 parser.add_argument("--t3d_weights", type=str, default='./T3D_saved_model_weights.hdf5', help="2d->3d transfer model")
+parser.add_argument("--lr", type=int, default=0.01, help="Learning rate")
 
 
 
@@ -74,7 +75,8 @@ def train():
 
     # compile model
     # optim = Adam(lr=1e-3, decay=1e-6)
-    optim = SGD(lr = 0.01, momentum=0.9, decay=1e-4, nesterov=True)
+    print("Learning rate {}".format(params.lr))
+    optim = SGD(lr = params.lr, momentum=0.9, decay=1e-4, nesterov=True)
     model.compile(optimizer=optim, loss='categorical_crossentropy', metrics=['accuracy'])
     
     if os.path.exists('./T3D_saved_model_weights.hdf5'):
